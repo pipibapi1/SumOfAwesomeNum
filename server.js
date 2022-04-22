83f51fa82bc118f838efbb4f1c302b104712e2b6
pipibapi1 <pipibapi1@gmail.com>
2022-04-22 19:35:30 +0700

    add bodyParser

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 //ROUTES
 
